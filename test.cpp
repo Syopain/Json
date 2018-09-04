fb25ca8ac91eafbd2d41a565885900bdbc30ba99
Syo Pain <syopain@qq.com>
2018-09-04 20:01:29 +0800

    update

--- test.cpp
+++ test.cpp
@@ -45,25 +45,25 @@ static void test_parse_literal()
 
 static void test_parse_number()
 {
-    TEST_NUMBER(0.0, "0");
-    TEST_NUMBER(0.0, "-0");
-    TEST_NUMBER(0.0, "-0.0");
-    TEST_NUMBER(1.0, "1");
-    TEST_NUMBER(-1.0, "-1");
-    TEST_NUMBER(1.5, "1.5");
-    TEST_NUMBER(-1.5, "-1.5");
-    TEST_NUMBER(3.1416, "3.1416");
-    TEST_NUMBER(1E10, "1E10");
-    TEST_NUMBER(1e10, "1e10");
-    TEST_NUMBER(1E+10, "1E+10");
-    TEST_NUMBER(1E-10, "1E-10");
-    TEST_NUMBER(-1E10, "-1E10");
-    TEST_NUMBER(-1e10, "-1e10");
-    TEST_NUMBER(-1E+10, "-1E+10");
-    TEST_NUMBER(-1E-10, "-1E-10");
-    TEST_NUMBER(1.234E+10, "1.234E+10");
-    TEST_NUMBER(1.234E-10, "1.234E-10");
-    TEST_NUMBER(0.0, "1e-10000"); /* must underflow */
+	TEST_NUMBER(0.0, "0");
+	TEST_NUMBER(0.0, "-0");
+	TEST_NUMBER(0.0, "-0.0");
+	TEST_NUMBER(1.0, "1");
+	TEST_NUMBER(-1.0, "-1");
+	TEST_NUMBER(1.5, "1.5");
+	TEST_NUMBER(-1.5, "-1.5");
+	TEST_NUMBER(3.1416, "3.1416");
+	TEST_NUMBER(1E10, "1E10");
+	TEST_NUMBER(1e10, "1e10");
+	TEST_NUMBER(1E+10, "1E+10");
+	TEST_NUMBER(1E-10, "1E-10");
+	TEST_NUMBER(-1E10, "-1E10");
+	TEST_NUMBER(-1e10, "-1e10");
+	TEST_NUMBER(-1E+10, "-1E+10");
+	TEST_NUMBER(-1E-10, "-1E-10");
+	TEST_NUMBER(1.234E+10, "1.234E+10");
+	TEST_NUMBER(1.234E-10, "1.234E-10");
+	TEST_NUMBER(0.0, "1e-10000"); /* must underflow */
 }
 
 #define TEST_STRING(expect, content)\
@@ -80,18 +80,18 @@ static void test_parse_number()
 static void test_parse_string()
 {
 
-    TEST_STRING("", "\"\"");
-    TEST_STRING("Hello", "\"Hello\"");
-    
-    TEST_STRING("Hello\nWorld", "\"Hello\\nWorld\"");
-    TEST_STRING("\" \\ / \b \f \n \r \t", "\"\\\" \\\\ \\/ \\b \\f \\n \\r \\t\"");
+	TEST_STRING("", "\"\"");
+	TEST_STRING("Hello", "\"Hello\"");
 
-    TEST_STRING("Hello\0World", "\"Hello\\u0000World\"");
-    TEST_STRING("\x24", "\"\\u0024\"");         /* Dollar sign U+0024 */
-    TEST_STRING("\xC2\xA2", "\"\\u00A2\"");     /* Cents sign U+00A2 */
-    TEST_STRING("\xE2\x82\xAC", "\"\\u20AC\""); /* Euro sign U+20AC */
-    TEST_STRING("\xF0\x9D\x84\x9E", "\"\\uD834\\uDD1E\"");  /* G clef sign U+1D11E */
-    TEST_STRING("\xF0\x9D\x84\x9E", "\"\\ud834\\udd1e\"");  /* G clef sign U+1D11E */
+	TEST_STRING("Hello\nWorld", "\"Hello\\nWorld\"");
+	TEST_STRING("\" \\ / \b \f \n \r \t", "\"\\\" \\\\ \\/ \\b \\f \\n \\r \\t\"");
+
+	TEST_STRING("Hello\0World", "\"Hello\\u0000World\"");
+	TEST_STRING("\x24", "\"\\u0024\"");         /* Dollar sign U+0024 */
+	TEST_STRING("\xC2\xA2", "\"\\u00A2\"");     /* Cents sign U+00A2 */
+	TEST_STRING("\xE2\x82\xAC", "\"\\u20AC\""); /* Euro sign U+20AC */
+	TEST_STRING("\xF0\x9D\x84\x9E", "\"\\uD834\\uDD1E\"");  /* G clef sign U+1D11E */
+	TEST_STRING("\xF0\x9D\x84\x9E", "\"\\ud834\\udd1e\"");  /* G clef sign U+1D11E */
 }
 
 static void test_parse_array()
@@ -204,21 +204,21 @@ static void test_parse_expect_value()
 
 static void test_parse_invalid_value()
 {
-    TEST_ERROR("parse invalid value", "nul");
-    TEST_ERROR("parse invalid value", "!?");
+	TEST_ERROR("parse invalid value", "nul");
+	TEST_ERROR("parse invalid value", "!?");
 
-    TEST_ERROR("parse invalid value", "+0");
-    TEST_ERROR("parse invalid value", "+1");
-    TEST_ERROR("parse invalid value", ".123"); /* at least one digit before '.' */
-    TEST_ERROR("parse invalid value", "1.");   /* at least one digit after '.' */
-    TEST_ERROR("parse invalid value", "INF");
-    TEST_ERROR("parse invalid value", "inf");
-    TEST_ERROR("parse invalid value", "NAN");
-    TEST_ERROR("parse invalid value", "nan");
+	TEST_ERROR("parse invalid value", "+0");
+	TEST_ERROR("parse invalid value", "+1");
+	TEST_ERROR("parse invalid value", ".123"); /* at least one digit before '.' */
+	TEST_ERROR("parse invalid value", "1.");   /* at least one digit after '.' */
+	TEST_ERROR("parse invalid value", "INF");
+	TEST_ERROR("parse invalid value", "inf");
+	TEST_ERROR("parse invalid value", "NAN");
+	TEST_ERROR("parse invalid value", "nan");
 
 #if 1
-    TEST_ERROR("parse invalid value", "[1,]");
-    TEST_ERROR("parse invalid value", "[\"a\", nul]");
+	TEST_ERROR("parse invalid value", "[1,]");
+	TEST_ERROR("parse invalid value", "[\"a\", nul]");
 #endif
 }
 
@@ -242,59 +242,59 @@ static void test_parse_number_too_big()
 
 static void test_parse_missing_quotation_mark()
 {
-    TEST_ERROR("parse miss quotation mark", "\"");
-    TEST_ERROR("parse miss quotation mark", "\"abc");
+	TEST_ERROR("parse miss quotation mark", "\"");
+	TEST_ERROR("parse miss quotation mark", "\"abc");
 }
 
 static void test_parse_invalid_string_escape()
 {
 #if 1
-   TEST_ERROR("parse invalid string escape", "\"\\v\"");
-   TEST_ERROR("parse invalid string escape", "\"\\'\"");
-   TEST_ERROR("parse invalid string escape", "\"\\0\"");
-   TEST_ERROR("parse invalid string escape", "\"\\x12\"");
+	TEST_ERROR("parse invalid string escape", "\"\\v\"");
+	TEST_ERROR("parse invalid string escape", "\"\\'\"");
+	TEST_ERROR("parse invalid string escape", "\"\\0\"");
+	TEST_ERROR("parse invalid string escape", "\"\\x12\"");
 #endif
 }
 
 static void test_parse_invalid_string_char()
 {
 #if 1
-    TEST_ERROR("parse invalid string char", "\"\x01\"");
-    TEST_ERROR("parse invalid string char", "\"\x1F\"");
+	TEST_ERROR("parse invalid string char", "\"\x01\"");
+	TEST_ERROR("parse invalid string char", "\"\x1F\"");
 #endif
 }
 
 static void test_parse_invalid_unicode_hex()
 {
-    TEST_ERROR("parse invalid unicode hex", "\"\\u\"");
-    TEST_ERROR("parse invalid unicode hex", "\"\\u0\"");
-    TEST_ERROR("parse invalid unicode hex", "\"\\u01\"");
-    TEST_ERROR("parse invalid unicode hex", "\"\\u012\"");
-    TEST_ERROR("parse invalid unicode hex", "\"\\u/000\"");
-    TEST_ERROR("parse invalid unicode hex", "\"\\uG000\"");
-    TEST_ERROR("parse invalid unicode hex", "\"\\u0/00\"");
-    TEST_ERROR("parse invalid unicode hex", "\"\\u0G00\"");
-    TEST_ERROR("parse invalid unicode hex", "\"\\u0/00\"");
-    TEST_ERROR("parse invalid unicode hex", "\"\\u00G0\"");
-    TEST_ERROR("parse invalid unicode hex", "\"\\u000/\"");
-    TEST_ERROR("parse invalid unicode hex", "\"\\u000G\"");
-    TEST_ERROR("parse invalid unicode hex", "\"\\u 123\"");
+	TEST_ERROR("parse invalid unicode hex", "\"\\u\"");
+	TEST_ERROR("parse invalid unicode hex", "\"\\u0\"");
+	TEST_ERROR("parse invalid unicode hex", "\"\\u01\"");
+	TEST_ERROR("parse invalid unicode hex", "\"\\u012\"");
+	TEST_ERROR("parse invalid unicode hex", "\"\\u/000\"");
+	TEST_ERROR("parse invalid unicode hex", "\"\\uG000\"");
+	TEST_ERROR("parse invalid unicode hex", "\"\\u0/00\"");
+	TEST_ERROR("parse invalid unicode hex", "\"\\u0G00\"");
+	TEST_ERROR("parse invalid unicode hex", "\"\\u0/00\"");
+	TEST_ERROR("parse invalid unicode hex", "\"\\u00G0\"");
+	TEST_ERROR("parse invalid unicode hex", "\"\\u000/\"");
+	TEST_ERROR("parse invalid unicode hex", "\"\\u000G\"");
+	TEST_ERROR("parse invalid unicode hex", "\"\\u 123\"");
 }
 
 static void test_parse_invalid_unicode_surrogate() {
-    TEST_ERROR("parse invalid unicode surrogate", "\"\\uD800\"");
-    TEST_ERROR("parse invalid unicode surrogate", "\"\\uDBFF\"");
-    TEST_ERROR("parse invalid unicode surrogate", "\"\\uD800\\\\\"");
-    TEST_ERROR("parse invalid unicode surrogate", "\"\\uD800\\uDBFF\"");
-    TEST_ERROR("parse invalid unicode surrogate", "\"\\uD800\\uE000\"");
+	TEST_ERROR("parse invalid unicode surrogate", "\"\\uD800\"");
+	TEST_ERROR("parse invalid unicode surrogate", "\"\\uDBFF\"");
+	TEST_ERROR("parse invalid unicode surrogate", "\"\\uD800\\\\\"");
+	TEST_ERROR("parse invalid unicode surrogate", "\"\\uD800\\uDBFF\"");
+	TEST_ERROR("parse invalid unicode surrogate", "\"\\uD800\\uE000\"");
 }
 
 static void test_parse_miss_comma_or_square_bracket() {
 #if 1
-    TEST_ERROR("parse miss comma or square bracket", "[1");
-    TEST_ERROR("parse miss comma or square bracket", "[1}");
-    TEST_ERROR("parse miss comma or square bracket", "[1 2");
-    TEST_ERROR("parse miss comma or square bracket", "[[]");
+	TEST_ERROR("parse miss comma or square bracket", "[1");
+	TEST_ERROR("parse miss comma or square bracket", "[1}");
+	TEST_ERROR("parse miss comma or square bracket", "[1 2");
+	TEST_ERROR("parse miss comma or square bracket", "[[]");
 #endif
 }
 
@@ -325,22 +325,22 @@ static void test_parse_miss_comma_or_curly_bracket()
 }
 
 static void test_parse() {
-    test_parse_literal();
-    test_parse_number();
-    test_parse_string();
-    test_parse_array();
+	test_parse_literal();
+	test_parse_number();
+	test_parse_string();
+	test_parse_array();
 	test_parse_object();
 
-    test_parse_expect_value();
-    test_parse_invalid_value();
-    test_parse_root_not_singular();
-    test_parse_number_too_big();
-    test_parse_missing_quotation_mark();
-    test_parse_invalid_string_escape();
-    test_parse_invalid_string_char();
-    test_parse_invalid_unicode_hex();
-    test_parse_invalid_unicode_surrogate();
-    test_parse_miss_comma_or_square_bracket();
+	test_parse_expect_value();
+	test_parse_invalid_value();
+	test_parse_root_not_singular();
+	test_parse_number_too_big();
+	test_parse_missing_quotation_mark();
+	test_parse_invalid_string_escape();
+	test_parse_invalid_string_char();
+	test_parse_invalid_unicode_hex();
+	test_parse_invalid_unicode_surrogate();
+	test_parse_miss_comma_or_square_bracket();
 	test_parse_miss_key();
 	test_parse_miss_colon;
 	test_parse_miss_comma_or_curly_bracket();
